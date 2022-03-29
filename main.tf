dc30673ba87c5393a6cdb44b69f1f474b813660c
guyeisenkot <68634672+guyeisenkot@users.noreply.github.com>
2022-03-29 16:19:25 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
-  region        = "us-west-2"
+  region        = "us-west-1"
   acl           = "public-read"
   force_destroy = true
   tags = {
